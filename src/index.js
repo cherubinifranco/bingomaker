6083b930f05c363d23ebc9505441921866109b0c
Cherubini Franco <cherubini.franco@hotmail.com>
2025-04-08 20:03:15 -0300

    chore: deleted stricMode for debugging

--- src/index.js
+++ src/index.js
@@ -4,11 +4,7 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <main className="[&::-webkit-scrollbar]:hidden root">
-      <div className="p-8">
-        <App />
-      </div>
-    </main>
-  </React.StrictMode>
+  <main className="[&::-webkit-scrollbar]:hidden root">
+    <App />
+  </main>
 );
