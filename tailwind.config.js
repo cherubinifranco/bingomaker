cc9a9e0961e37d6d845296488956d9e822f5f640
Cherubini Franco <cherubini.franco@hotmail.com>
2025-04-15 04:20:17 -0300

    Feat: Updated tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				stext: "#B0B9D1",
-				bg: "#28426B",
-				mainbg: "#172446",
-				sborder: "#94A7CD",
-				accent1: "#F87633",
-				accent2: "#FFC102",
-				accent3: "#01AFCC",
-			}
-		},
-	},
-	plugins: [],
-}
-
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        stext: "#B0B9D1",
+        bg: "#F5EFDD",
+        mainbg: "#EB9D32",
+        sborder: "#DF7D36",
+        accent1: "#3773A8",
+        accent1hover: "#244371",
+        accent2: "#D74533",
+        accent2hover: "#972C29",
+      },
+    },
+  },
+  plugins: [],
+};
