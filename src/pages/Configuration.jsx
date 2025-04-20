6f70501f292cfc143387e505a780812e69b4e96c
Cherubini Franco <cherubini.franco@hotmail.com>
2025-04-19 22:28:16 -0300

    Fix: mp3 files didnt load correctly

--- src/pages/Configuration.jsx
+++ src/pages/Configuration.jsx
@@ -1,44 +1,36 @@
 import { useState } from "react";
 import { getFile } from "../utils";
 export default function ConfigurationPage() {
-  const [soundState, updateSoundState] = useState(
-    JSON.parse(localStorage.getItem("soundState")) ?? true
-  );
-  const [trackWinnerState, updateTrackWinnerState] = useState(
-    JSON.parse(localStorage.getItem("trackingState")) ?? false
-  );
-  const [lapSound, updateLapSound] = useState(
-    localStorage.getItem("lapSoundRoute") ?? ""
-  );
-  const [restartSound, updateRestartSound] = useState(
-    localStorage.getItem("restartSoundRoute") ?? ""
-  );
-  const [winnerSound, updateWinnerSound] = useState(
-    localStorage.getItem("winnerSoundRoute") ?? ""
-  );
+  const [soundEnabled, updateSoundEnabled] = useState(JSON.parse(localStorage.getItem("soundEnabled")) ?? true);
+  const [trackWinnerState, updateTrackWinnerState] = useState(JSON.parse(localStorage.getItem("trackingState")) ?? false);
+  const [lapSound, updateLapSound] = useState(localStorage.getItem("lapSoundRoute") ?? "");
+  const [restartSound, updateRestartSound] = useState(localStorage.getItem("restartSoundRoute") ?? "");
+  const [winnerSound, updateWinnerSound] = useState(localStorage.getItem("winnerSoundRoute") ?? "");
 
   function clear(callback) {
     const value = "";
     if (callback == "lapSoundRoute") {
-      localStorage.setItem("lapSoundRoute", value);
+      localStorage.setItem("lapSoundRoute", JSON.stringify(value));
       updateLapSound(value);
     }
     if (callback == "restartSoundRoute") {
-      localStorage.setItem("restartSoundRoute", value);
+      localStorage.setItem("restartSoundRoute", JSON.stringify(value));
       updateRestartSound(value);
     }
     if (callback == "winnerSoundRoute") {
-      localStorage.setItem("winnerSoundRoute", value);
+      localStorage.setItem("winnerSoundRoute", JSON.stringify(value));
       updateWinnerSound(value);
     }
   }
 
   async function handleFile(callback) {
     let value = await getFile();
-    value = value.split(".");
-    if (value[1] != "mp3") {
-      alert("Sound Files must be mp3");
-      value = "";
+    if (value != "") {
+      let svalue = value.split(".");
+      if (svalue[1] != "mp3") {
+        alert("Sound Files must be mp3");
+        value = "";
+      }
     }
     if (callback == "lapSoundRoute") {
       localStorage.setItem("lapSoundRoute", value);
@@ -54,9 +46,9 @@ export default function ConfigurationPage() {
     }
   }
 
-  function handleSoundState() {
-    updateSoundState(!soundState);
-    localStorage.setItem("soundState", JSON.stringify(!soundState));
+  function handleSoundEnabled() {
+    updateSoundEnabled(!soundEnabled);
+    localStorage.setItem("soundEnabled", JSON.stringify(!soundEnabled));
   }
 
   function handleTrackWinnerState() {
@@ -79,10 +71,7 @@ export default function ConfigurationPage() {
 
       <article className="py-4 bg-mainbg rounded-lg m-4">
         <div className="flex flex-row justify-between items-center pr-8">
-          <label
-            className="inline-block ps-[0.15rem] indent-4 hover:cursor-pointer w-36 text-nowrap pr-4 select-none"
-            htmlFor="trackWinnerState"
-          >
+          <label className="inline-block ps-[0.15rem] indent-4 hover:cursor-pointer w-36 text-nowrap pr-4 select-none" htmlFor="trackWinnerState">
             Track Winner
           </label>
           <div className="h-[1px] bg-slate-700 w-full"></div>
@@ -108,10 +97,7 @@ export default function ConfigurationPage() {
 
       <article className="py-4 bg-mainbg rounded-lg m-4">
         <div className="flex flex-row justify-between items-center pr-8">
-          <label
-            className="inline-block ps-[0.15rem] indent-4 hover:cursor-pointer w-36 text-nowrap pr-4 select-none"
-            htmlFor="soundState"
-          >
+          <label className="inline-block ps-[0.15rem] indent-4 hover:cursor-pointer w-36 text-nowrap pr-4 select-none" htmlFor="soundEnabled">
             Play Sounds
           </label>
           <div className="h-[1px] bg-slate-700 w-full"></div>
@@ -121,9 +107,9 @@ export default function ConfigurationPage() {
               className="rounded-lg me-2 mt-[0.3rem] h-3.5 w-8 relative appearance-none rounded-[0.4375rem] before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-accent2 after:shadow-switch-2 after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ms-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-accent1 checked:after:shadow-switch-1 checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-switch-3 focus:before:shadow-black/60 focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:before:ms-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-switch-3 checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] bg-white/25 after:bg-surface-dark checked:bg-primary checked:after:bg-primary"
               type="checkbox"
               role="switch"
-              id="soundState"
-              onClick={handleSoundState}
-              checked={soundState}
+              id="soundEnabled"
+              onClick={handleSoundEnabled}
+              checked={soundEnabled}
             />
           </div>
         </div>
@@ -134,18 +120,8 @@ export default function ConfigurationPage() {
           <label htmlFor="lapSound" className="indent-4 w-52">
             Lap Sound File
           </label>
-          <input
-            type="text"
-            id="lapSound"
-            className="mx-4 w-full bg-mainbg cursor-pointer"
-            readOnly
-            value={lapSound}
-            onClick={() => handleFile("lapSoundRoute")}
-          />
-          <button
-            onClick={() => clear("lapSoundRoute")}
-            className="w-10 rounded-lg text-center hover:bg-red-400 mx-4"
-          >
+          <input type="text" id="lapSound" className="mx-4 w-full bg-mainbg cursor-pointer" readOnly value={lapSound} onClick={() => handleFile("lapSoundRoute")} />
+          <button onClick={() => clear("lapSoundRoute")} className="w-10 rounded-lg text-center hover:bg-red-400 mx-4">
             X
           </button>
         </div>
@@ -156,18 +132,8 @@ export default function ConfigurationPage() {
           <label htmlFor="lapSound" className="indent-4 w-52">
             Restart Sound File
           </label>
-          <input
-            type="text"
-            id="lapSound"
-            className="mx-4 w-full bg-mainbg cursor-pointer"
-            readOnly
-            value={restartSound}
-            onClick={() => handleFile("restartSoundRoute")}
-          />
-          <button
-            onClick={() => clear("restartSoundRoute")}
-            className="w-10 rounded-lg text-center hover:bg-red-400 mx-4"
-          >
+          <input type="text" id="lapSound" className="mx-4 w-full bg-mainbg cursor-pointer" readOnly value={restartSound} onClick={() => handleFile("restartSoundRoute")} />
+          <button onClick={() => clear("restartSoundRoute")} className="w-10 rounded-lg text-center hover:bg-red-400 mx-4">
             X
           </button>
         </div>
@@ -178,18 +144,8 @@ export default function ConfigurationPage() {
           <label htmlFor="lapSound" className="indent-4 w-52">
             Winner Sound File
           </label>
-          <input
-            type="text"
-            id="lapSound"
-            className="mx-4 w-full bg-mainbg cursor-pointer"
-            readOnly
-            value={winnerSound}
-            onClick={() => handleFile("winnerSoundRoute")}
-          />
-          <button
-            onClick={() => clear("winnerSoundRoute")}
-            className="w-10 rounded-lg text-center hover:bg-red-400 mx-4"
-          >
+          <input type="text" id="lapSound" className="mx-4 w-full bg-mainbg cursor-pointer" readOnly value={winnerSound} onClick={() => handleFile("winnerSoundRoute")} />
+          <button onClick={() => clear("winnerSoundRoute")} className="w-10 rounded-lg text-center hover:bg-red-400 mx-4">
             X
           </button>
         </div>
